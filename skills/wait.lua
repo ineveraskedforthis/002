30400caba9cd951c36b4d1a293fdcb2ba6f948ad
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2025-08-19 21:38:48 +0300

    remove stray spaces

--- skills/wait.lua
+++ skills/wait.lua
@@ -7,7 +7,7 @@ return {
 	effects_sequence = {
 	},
 	targeted = false,
-        required_elements = {},
-        required_magic = 0,
-        required_strength = 0
+	required_elements = {},
+	required_magic = 0,
+	required_strength = 0
 }
